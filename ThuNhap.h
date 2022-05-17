26c75644e9d5c98ef7f24d8634587862c732a250
ntclai <105714319+ntclai@users.noreply.github.com>
2022-05-17 20:45:50 +0700

    Update ThuNhap.h
    Last pull

--- ThuNhap.h
+++ ThuNhap.h
@@ -1,5 +1,22 @@
 #pragma once
+#ifndef ThuNhap_h
+#define ThuNhap_h
+#include<iostream>
+using namespace std;
+
 class ThuNhap
 {
+private:
+	double luong;
+	double thuNhapKhac;
+public:
+	ThuNhap();
+	ThuNhap(double,double);
+	void nhapLuong();
+	void nhapThuNhapKhac();
+	double layLuong();
+	double layThuNhapKhac();
+	~ThuNhap();
 };
 
+#endif // !ThuNhap_h
